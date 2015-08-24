deff35d893ea417ff6b2863f5a0b462e5135c589
visualiz3 <erictan.webdeveloper@gmail.com>
2015-08-24 10:27:51 +0800

    add map window

--- panic.py
+++ panic.py
@@ -30,10 +30,18 @@ class GuiPart:
         master.title("DF Panic Alarm")
         master.geometry("+300+30")
 
+
         # create a toplevel menu
         menubar = Menu(master)
-        menubar.add_command(label="Quit", command=endCommand)
-        menubar.add_command(label="Clear DB", command=self.clearDatabase)
+        filemenu = Menu(menubar, tearoff=0)
+        filemenu.add_command(label="Clear DB", command=self.clearDatabase)
+        filemenu.add_separator()
+        filemenu.add_command(label="Exit", command=endCommand)
+        menubar.add_cascade(label="File", menu=filemenu)
+        centralMenu = Menu(menubar, tearoff=0)
+        centralMenu.add_command(label="MCU Reset", command=self.mcuReset )
+        centralMenu.add_command(label="Check MCU ID", command=self.mcuIDChecking)
+        menubar.add_cascade(label="Central Menu" ,menu=centralMenu )
 
         # display the menu
         master.config(menu=menubar)
@@ -48,19 +56,26 @@ class GuiPart:
 
 
         # Top Frame Buttons
-        b1 = Button(topFrame,text="Configure Central ID" ,command=self.configCentralId )
-        b1.grid(row=0,column=0)
-        b2 = Button(topFrame,text="Ask Respond", command=self.askRespond )
-        b2.grid(row=0,column=1)
-        b3 = Button(topFrame,text="Repeater Search Path", command=self.repeaterSearchPath )
-        b3.grid(row=0,column=3)
-        b4 = Button(topFrame,text="All Repeater Search Path", command=self.allRepeaterSearchPath )
-        b4.grid(row=0,column=4)
-        b5 = Button(topFrame,text="Check Central ID", command=self.mcuIDChecking )
-        b5.grid(row=0,column=5, sticky=E)
+        buttonwidth = 20
+        b1 = Button(topFrame,text="Configure Central ID" ,command=self.configCentralId , width=buttonwidth)
+        b1.grid(row=0,column=0, sticky=W)
+        b2 = Button(topFrame,text="Ask Respond", command=self.askRespond , width=buttonwidth )
+        b2.grid(row=0,column=1, sticky=W)
+        b3 = Button(topFrame,text="Repeater Search Path", command=self.repeaterSearchPath , width=buttonwidth)
+        b3.grid(row=0,column=2, sticky=W)
+        b4 = Button(topFrame,text="All Repeater Search Path", command=self.allRepeaterSearchPath, width=buttonwidth )
+        b4.grid(row=0,column=3, sticky=W)
+        b5 = Button(topFrame,text="Check Central ID", command=self.mcuIDChecking, width=buttonwidth )
+        b5.grid(row=0,column=4, sticky=W)
+        b6 = Button(topFrame,text="Check Repeater Central ID", command=self.repeaterCheckCentralID , width=buttonwidth)
+        b6.grid(row=1,column=0, sticky=W)
+        b7 = Button(topFrame,text="Check Repeater Path", command=self.repeaterCheckPath , width=buttonwidth)
+        b7.grid(row=1,column=1, sticky=W)
+        b8 = Button(topFrame,text="Map", command=self.openMap , width=buttonwidth)
+        b8.grid(row=1,column=2, sticky=W)
 
         #Initialize variables for UI
-        listbox_width = 51
+        listbox_width = 40
 
         # Middle Frame
         # Receive
@@ -73,13 +88,13 @@ class GuiPart:
         scrollbar.config( command = self.l1.yview)
         
         # Send
-        lbl2 = Label(middleFrame, text="Send")
-        lbl2.grid(row=0,column=2)
-        scrollbar2 = Scrollbar(middleFrame)
-        self.l2 = Listbox(middleFrame, width=listbox_width,yscrollcommand=scrollbar2.set)
-        self.l2.grid(row=1,column=2)
-        scrollbar2.grid(row=1,column=3,sticky=N+S)
-        scrollbar2.config( command = self.l2.yview)
+        # lbl2 = Label(middleFrame, text="Send")
+        # lbl2.grid(row=0,column=2)
+        # scrollbar2 = Scrollbar(middleFrame)
+        # self.l2 = Listbox(middleFrame, width=listbox_width,yscrollcommand=scrollbar2.set)
+        # self.l2.grid(row=1,column=2)
+        # scrollbar2.grid(row=1,column=3,sticky=N+S)
+        # scrollbar2.config( command = self.l2.yview)
         
         # Bottom Frame 
         # Console logging
@@ -88,15 +103,16 @@ class GuiPart:
         self.log.grid(row=0,column=0)
         scrollbar3.grid(row=0,column=1,sticky=N+S)
         scrollbar3.config( command = self.log.yview)
-        b6 = Button(bottomFrame,text="Clear log", command=self.clearLogger )
-        b6.grid(row=1,column=0, sticky=W)
+        clearlog_button = Button(bottomFrame,text="Clear log", command=self.clearLogger )
+        clearlog_button.grid(row=1,column=0, sticky=W)
 
         master.protocol('WM_DELETE_WINDOW', self.on_exit)
         master.resizable(0,0)
 
-        
+
         for repeater in self.table:
             self.l1.insert(END, repeater['repeater'])
+
 
     def logger(self, msg):
         self.log.insert(END, msg)
@@ -119,17 +135,29 @@ class GuiPart:
     def repeaterSearchPath(self):
         currentValue = self.l1.get(self.l1.curselection())
         self.send(b"ART"+ currentValue +"S000\r")
-        msg = "Asking Device ID=" + currentValue + " to search path...\n"
+        msg = "Asking Device ID=" + currentValue + " to search path... Please wait 1 minute..\n"
         self.logger(msg)
 
     def allRepeaterSearchPath(self):
         self.send(b"ART00000000S000\r")
-        self.logger("Asking all repeater to search path... Please wait for 1 minute for reply..")
+        self.logger("Asking all repeater to search path... Please wait for reply..\n")
 
     def mcuIDChecking(self):
         self.send(b"ARI\r")
         # msg = "Central ID=" + currentValue + " to respond...\n" 
         # self.logger(msg)
+    
+    def repeaterCheckCentralID(self):
+        currentValue = self.l1.get(self.l1.curselection())
+        self.send(b"ART"+ currentValue + "F\r")
+        msg = "Checking Repeater Central ID = " + currentValue + " ..\n"
+        self.logger(msg)
+
+    def repeaterCheckPath(self):
+        currentValue = self.l1.get(self.l1.curselection())
+        self.send(b"ART"+ currentValue + "U\r")
+        msg = "Checking Repeater Path = " + currentValue + " ..\n"
+        self.logger(msg)
     
     def clearDatabase(self):
         try:
@@ -138,6 +166,11 @@ class GuiPart:
         except:
             print "Database already drop and don't exist"
     
+    def mcuReset(self):
+        if tkMessageBox.askyesno("MCU Operation", "Do you want to reset the MCU?"):
+            self.send(b"ARR\r")
+            self.logger("MCU Reset..\n")
+
     def decode(self,b):
 
         m = re.match('RA(\w)(\d{8})(.{2})?', b)
@@ -149,20 +182,50 @@ class GuiPart:
             RSSI = m.group(3)
 
             if cmd == 'A':
-                msg = "Repeater with ID="+ repeater + " has acknowledged..\n"
+                msg = "Repeater with ID = "+ repeater + " has acknowledged..\n"
                 self.logger(msg)
             elif cmd == "I":
-                msg = "Central ID=" + repeater + "\n"
+                msg = "Central ID = " + repeater + "\n"
                 self.logger(msg)
                 if not self.table.find_one(repeater=repeater):
                     self.l1.insert(END, repeater)
                     self.table.insert(dict(repeater=repeater))
-            elif cmd == "C":
-                msg = "Repeater with ID=" + repeater + " has responded or finished searching..\n"
+            elif cmd == "E":
+                msg = "Repeater with ID = " + repeater + " has responded..\n"
                 self.logger(msg)
+            elif cmd == "C":
+                msg = "Repeater with ID = " + repeater + " has finished searching path..\n"
+                self.logger(msg)
+                
+                if 'searchedPath' in dir(self):
+                    self.searchedPath.append(repeater)
+                    print self.searchedPath
+                    listofRepeaters = self.l1.get(0, END)
+                    diff = list(set(listofRepeaters) - set(self.searchedPath))
+                    for i in range(len(self.l1.get(0, END))):
+                        self.l1.itemconfig(i, {'bg' : 'green'})
+                    for repeater in diff:
+                        index = self.getIndexOfListbox(repeater)
+                        self.l1.itemconfig(index, {'bg':'red'})         
+
+                else:
+                    self.searchedPath = []
+                    self.searchedPath.append(repeater)
+            elif cmd == "F":
+                msg = "Repeater central ID = " + repeater + " ..\n"
+                self.logger(msg)
+            elif int(cmd) in range(1,4):
+                msg = "Repeater path " + cmd + " = " + repeater + " ..\n"
+                self.logger(msg)
+
 
         else:
             print 'Cant decode', b
+
+    def getIndexOfListbox(self,item):
+        for i in range(len(self.l1.get(0, END))):
+            if self.l1.get(i) == item:
+                return i
 
     def on_exit(self):
         """When you click to exit, this function is called"""
@@ -183,6 +246,11 @@ class GuiPart:
                 self.decode(msg)
             except Queue.Empty:
                 pass
+
+    def openMap(self):
+        mapWindow = Toplevel(self.master)
+        mapWindow.geometry("+300+30")
+
 
 class ThreadedClient:
     """
@@ -235,10 +303,15 @@ class ThreadedClient:
         One important thing to remember is that the thread has to yield
         control.
         """
+
+        time.sleep(1) # cheat the program to let UI finish loading
+
+
         self.d = d2xx.open(0)
         self.d.setBaudRate(115200)
         self.d.setTimeouts(1, 0)
-        time.sleep(1) # cheat the program to let UI finish loading
+
+        
         buffer = ''
         try:
             while self.running:
